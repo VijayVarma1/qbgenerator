4038234f7f28d3e3e968982ffe56a1707cce4126
Gaming-PC <verma.vijay22@gmail.com>
2025-07-16 21:20:34 +0530

    1st update

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>QB Generator</h1>
+            <p>Welcome to the Electron + React app.</p>
+        </div>
+    );
+}
+
+export default App;
